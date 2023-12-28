83f82d2f5d64e5f35967e25627316a600434081c
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-12-28 20:10:42 +0100

    Pushing the latest changes of home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,14 @@
-<script>
-  import { user } from "$lib/firebase";
-  import { auth } from "$lib/firebase";
-  import { signOut } from "firebase/auth";
-</script>
-
 <header class="fixed top-5 left-0 w-full px-10">
   <nav class="navbar bg-slate-100 rounded-full px-4">
     <div class="flex-1">
       <span class="btn btn-ghost text-xl text-black md:text-2xl">Linkee</span>
     </div>
 
-    {#if $user}
-      <button
-        on:click={() => signOut(auth)}
-        class="btn btn-primary text-white px-10 rounded-full md:text-lg"
-        >Logout</button
-      >{:else}
-      <a
-        href="/login"
-        class="btn btn-primary text-white px-10 rounded-full md:text-lg"
-        >Login</a
-      >
-    {/if}
+    <a
+      href="/login"
+      class="btn btn-primary text-white px-10 rounded-full md:text-lg"
+      >Get Started</a
+    >
   </nav>
 </header>
 
