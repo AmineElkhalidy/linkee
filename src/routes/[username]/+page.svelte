9bc5c9cadc3765c7454e8cf9669bdc639282f755
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-12-30 12:20:18 +0100

    Adding new improvements...

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import UserLink from "$lib/components/UserLink.svelte";
   import type { PageData } from "./$types";
+  import { userData } from "$lib/firebase";
+
+  $: href = `/${$userData?.username}/edit`;
 
   export let data: PageData;
 </script>
@@ -28,12 +31,16 @@
     {data.bio ?? "No bio yet..."}
   </p>
 
-  <div class="">
+  <div>
     <p class="text-lg mb-2">Your social links:</p>
-    <ul class="list-none mb-6">
+    <ul class="list-none mb-6 flex flex-col justify-center items-center">
       {#each data.links as item}
         <UserLink {...item} />
       {/each}
     </ul>
+  </div>
+
+  <div class="flex justify-center items-center mb-8">
+    <a {href} class="btn btn-success px-10 text-white">Add Links</a>
   </div>
 </main>
